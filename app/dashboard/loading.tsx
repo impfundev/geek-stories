562854d46fac77493a01d664d3cae09c765dc9dd
impfundev <mail@ilhammaulana.me>
2024-04-25 16:35:41 +0700

    feat: loading dashboard

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,10 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <Skeleton className="w-full h-full flex flex-col items-center justify-center">
+      <Loader2 className="w-8 h-8 animate-spin" />
+    </Skeleton>
+  );
+}
